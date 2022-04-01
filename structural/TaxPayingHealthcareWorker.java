c92db3caab48eba9cdcf46ccad361604b33b5a52
Narawich Youbua <narawich49@gmail.com>
2022-04-01 16:16:56 +0700

    fin

--- structural/TaxPayingHealthcareWorker.java
+++ structural/TaxPayingHealthcareWorker.java
@@ -1,2 +1,15 @@
-package structural;public class TaxPayingHealthcareWorker {
+package structural;
+
+public class TaxPayingHealthcareWorker extends HealthcareWorkerDecorator{
+    public TaxPayingHealthcareWorker(HealthcareWorker worker) {
+        super(worker);
+        System.out.println("Decorate " + worker.getName() + " with TaxPaying.");
+    }
+
+    @Override
+    public double getPrice() {
+        double priceTaxed = 0.0;
+        priceTaxed = super.getPrice() * 1.1;
+        return priceTaxed;
+    }
 }
